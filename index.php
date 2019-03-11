1f13d857c69d899dc46fcccc38c935717ded4ee6
mohtth <48013101+mohtth@users.noreply.github.com>
2019-03-11 17:18:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
-
 $str_filmTitle = "Indiana Jones et la dernière croisade ";      // La première variable doit contenir le nom de ce film d'Indiana Jones, en VO
-echo $str_filmTitle;
-
 $int_year = 1989;                                               // La seconde variable te permettra d'indiquer si tu as vu, ou non, le film en question
-echo $int_year;
-
 $float_note = 8.9;                                              // La troisième sera l'année de sortie en salle.
-echo $float_note;
-
 $bool_Vu = true;                                                // La quatrième la note du film sur le site IMDB.
-echo $bool_Vu;
 
+
+echo "titre du film : ".$str_filmTitle."<br> année de sortie : ".$int_year." <br> note film : ".$float_note."/10 <br> film vu : ".$bool_Vu;
 ?>
